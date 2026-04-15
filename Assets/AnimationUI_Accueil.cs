aabb5b8744cfea2ec5dfe39eef5429a7869d7005
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2026-04-14 21:04:41 -0400

    script revert

--- Assets/AnimationUI_Accueil.cs
+++ Assets/AnimationUI_Accueil.cs
@@ -1,28 +1,25 @@
- using UnityEngine;
+using UnityEngine;
 
- public class AnimationUI_Accueil : MonoBehaviour
- {
-     public GameObject Menu;
-     public Transform MenuReference;
-     [SerializeField] LeanTweenType easetype;
+public class AnimationUI_Accueil : MonoBehaviour
+{
+    public GameObject Menu;
+    public Transform MenuReference;
+    [SerializeField] LeanTweenType easetype;
 
-     // Start is called once before the first execution of Update after the MonoBehaviour is created
-     void Start()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-     void Update()
-     {
-        BougerMenu();
-     }
+    void Update()
+    {
 
-    
-    
+    }
+
     public void BougerMenu()
     {
-        LeanTween.moveX(Menu, MenuReference.position.x , 2f). setEase(easetype);
+        LeanTween.moveX(Menu, MenuReference.position.x, 2f).setEase(easetype);
     }
-  
 }
