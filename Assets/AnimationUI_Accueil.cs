f6209cfb0208d05ced060c28d21d74007ee730ba
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2026-04-14 21:16:13 -0400

    revert

--- Assets/AnimationUI_Accueil.cs
+++ Assets/AnimationUI_Accueil.cs
@@ -6,18 +6,23 @@ public class AnimationUI_Accueil : MonoBehaviour
     public Transform MenuReference;
     [SerializeField] LeanTweenType easetype;
 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
     }
 
+    // Update is called once per frame
     void Update()
     {
-
+        BougerMenu();
     }
+
+
 
     public void BougerMenu()
     {
         LeanTween.moveX(Menu, MenuReference.position.x, 2f).setEase(easetype);
     }
+
 }
