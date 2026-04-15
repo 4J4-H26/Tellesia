bf9d5410e761f93472d6de07b22cc9e95d58c6d2
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2026-04-14 21:07:43 -0400

    revert

--- Assets/AnimationUI_Accueil.cs
+++ Assets/AnimationUI_Accueil.cs
@@ -6,13 +6,11 @@ public class AnimationUI_Accueil : MonoBehaviour
     public Transform MenuReference;
     [SerializeField] LeanTweenType easetype;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
 
