36750397339387cce8674126c8b326b06130d173
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2026-04-14 20:57:48 -0400

    fichier accueil

--- Assets/AnimationUI_Accueil.cs
+++ Assets/AnimationUI_Accueil.cs
@@ -1,28 +1,28 @@
-// using UnityEngine;
+ using UnityEngine;
 
-// public class AnimationUI_Accueil : MonoBehaviour
-// {
-//     public GameObject Menu;
-//     public Transform MenuReference;
-//     [SerializeField] LeanTweenType easetype;
+ public class AnimationUI_Accueil : MonoBehaviour
+ {
+     public GameObject Menu;
+     public Transform MenuReference;
+     [SerializeField] LeanTweenType easetype;
 
-//     // Start is called once before the first execution of Update after the MonoBehaviour is created
-//     void Start()
-//     {
+     // Start is called once before the first execution of Update after the MonoBehaviour is created
+     void Start()
+    {
         
-//     }
+    }
 
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         BougerMenu();
-//     }
+    // Update is called once per frame
+     void Update()
+     {
+        BougerMenu();
+     }
 
     
     
-//     public void BougerMenu()
-//     {
-//         LeanTween.moveX(Menu, MenuReference.position.x , 2f). setEase(easetype);
-//     }
+    public void BougerMenu()
+    {
+        LeanTween.moveX(Menu, MenuReference.position.x , 2f). setEase(easetype);
+    }
   
-// }
+}
