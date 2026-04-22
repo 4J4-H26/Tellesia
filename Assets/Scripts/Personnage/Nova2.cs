48fa25ef62485368fd13bb463fbd23f1b6a55644
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2026-04-21 21:39:25 -0400

    la console est vidée pour le niv 2

---
+++ Assets/Scripts/Personnage/Nova2.cs
@@ -0,0 +1,104 @@
+// script pour le perso de Nova niveau 2
+// auteur : sammuel
+// date : 21 Avril 2026
+
+// desc : ** Nova **
+
+using UnityEngine;
+
+public class Nova2 : MonoBehaviour
+{
+    [Header("Déplacement")]
+    public float vitesse = 2f;
+
+    [Header("Rotation")]
+    public float vitesseRotation = 360f;
+
+    private bool canMove = true;
+    private Vector3 direction;
+    private Animator anim;
+    private Transform cam;
+    private Rigidbody rb;
+
+    private bool autoMove = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        cam = Camera.main.transform;
+        rb = GetComponent<Rigidbody>();
+        anim.applyRootMotion = false;
+    }
+
+    void Update()
+    {
+        if (!canMove)
+        {
+            anim.SetBool("enMarche", false);
+            direction = Vector3.zero;
+            return;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 forward = cam.forward;
+        Vector3 right = cam.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        direction = (forward * vertical + right * horizontal).normalized;
+
+        bool estEnMarche = direction.sqrMagnitude > 0.01f;
+        anim.SetBool("enMarche", estEnMarche);
+
+        if (estEnMarche)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+            transform.rotation = Quaternion.RotateTowards(
+                transform.rotation,
+                targetRotation,
+                vitesseRotation * Time.deltaTime
+            );
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (autoMove)
+        {
+            Vector3 move = transform.forward * vitesse * Time.fixedDeltaTime;
+            rb.MovePosition(rb.position + move);
+            anim.SetBool("enMarche", true);
+            return;
+        }
+
+        if (!canMove) return;
+
+        Vector3 moveNormal = direction * vitesse * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + new Vector3(moveNormal.x, 0, moveNormal.z));
+    }
+
+    public void SetCanMove(bool value)
+    {
+        canMove = value;
+    }
+
+    public bool IsMovementLocked()
+    {
+        return !canMove;
+    }
+
+    public void ActiverAutoMove()
+    {
+        canMove = false;
+        autoMove = true;
+        anim.SetTrigger("Sortie");
+        FindFirstObjectByType<ChangementCam>().ActiverCameraCinematique();
+    }
+}
