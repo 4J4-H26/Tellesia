813cd22534e0587ea20f9717abd41c16eaa5a0f1
david <davidphung1242@gmail.com>
2026-05-05 23:43:11 -0400

    Commencement Page Reglage Start Menu

--- Assets/scripts_David/AnimationUI_Accueil.cs
+++ Assets/scripts_David/AnimationUI_Accueil.cs
@@ -1,1 +1,36 @@
-﻿
+﻿// using UnityEngine;
+using UnityEngine;
+
+// public class AnimationUI_Accueil : MonoBehaviour
+// {
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
+//         animationMenuBouttons;
+//     }
+public class AnimationUI_Accueil : MonoBehaviour
+{
+    public GameObject Menu;
+    public Transform MenuReference;
+    [SerializeField] LeanTweenType easetype;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        BougerMenu();
+    }
+
+
+
+    public void BougerMenu()
+    {
+        LeanTween.moveX(Menu, MenuReference.position.x, 2f).setEase(easetype);
+    }
+
+}
