0d5461ec1547a029b3c5122ce6880d26793578bb
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2026-05-06 11:25:37 -0400

    puzzle du code

--- Assets/Puzzle8DuCode.cs
+++ Assets/Puzzle8DuCode.cs
@@ -5,27 +5,24 @@ public class Puzzle8DuCode : MonoBehaviour
     [Header("Canvas du puzzle 8")]
     public GameObject CanvasPuzzle8;
 
+    [Header("Image Cube 1")]
+    public GameObject Cube1;
 
-    [Header("Images Cube 1")]
-    public GameObject imagedefaut;
+    [Header("Image Cube 2")]
+    public GameObject Cube2;
 
-    public GameObject image1;
-    public GameObject image2;
-    public GameObject image3;
-    public GameObject image4;
-    public GameObject image5;
-    public GameObject image6;
+    [Header("Image Cube 3")]
+    public GameObject Cube3;
 
-    [Header("Images Cube 2")]
-    public GameObject imagedefaut2;
+    [Header("Image Cube 4")]
+    public GameObject Cube4;
 
-    public GameObject image7;
-    public GameObject image8;
-    public GameObject image9;
-    public GameObject image10;
-    public GameObject image11;
-    public GameObject image12;
+    [Header("Cubes gris")]
+    public GameObject CubeGris;
+    public GameObject CubeGris2;
 
+    [Header("Animation cube")]
+    public GameObject AnimationCube;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -36,6 +33,6 @@ public class Puzzle8DuCode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
