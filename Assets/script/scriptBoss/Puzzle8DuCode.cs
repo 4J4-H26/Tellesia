aacf68547aab07b6171696b7e63af653a028268f
stella-roy <e2475620@cmaisonneuve.qc.ca>
2026-05-20 09:41:57 -0400

    intégration des timers

--- Assets/script/scriptBoss/Puzzle8DuCode.cs
+++ Assets/script/scriptBoss/Puzzle8DuCode.cs
@@ -1,152 +1,164 @@
-//// script pour le puzzle du code du niveau 4
-//// auteur : sammuel
-//// date : 6 Mai 2026
+// script pour le puzzle du code du niveau 4
+// auteur : sammuel
+// date : 6 Mai 2026
 
-//// desc : ** ce script gère un puzzle de pige aléatoire.
-////           Nova doit faire apparaître Cube3 et Cube4
-////           en même temps pour réussir le puzzle. **
+// desc : ** ce script gère un puzzle de pige aléatoire.
+//           Nova doit faire apparaître Cube3 et Cube4
+//           en même temps pour réussir le puzzle. **
 
-//using UnityEngine;
-//using System.Collections;
-//using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-//public class Puzzle8DuCode : MonoBehaviour
-//{
-//    [Header("Canvas du puzzle 8")]
-//    public GameObject CanvasPuzzle8;
+public class Puzzle8DuCode : MonoBehaviour
+{
+    [Header("Canvas du puzzle 8")]
+    public GameObject CanvasPuzzle8;
 
-//    [Header("Image chiffres Cube 3")]
-//    public GameObject ChiffreCube3;
-//    public GameObject Chiffre2Cube3;
+    [Header("Image chiffres Cube 3")]
+    public GameObject ChiffreCube3;
+    public GameObject Chiffre2Cube3;
 
-//    [Header("Image chiffres Cube 4")]
-//    public GameObject ChiffreCube4;
-//    public GameObject Chiffre2Cube4;
+    [Header("Image chiffres Cube 4")]
+    public GameObject ChiffreCube4;
+    public GameObject Chiffre2Cube4;
 
-//    [Header("Image Cube 3")]
-//    public GameObject Cube3;
+    [Header("Boutons cliquables")]
+    public GameObject Chiffre7Cube3;
+    public GameObject Chiffre7Cube4;
 
-//    [Header("Image Cube 4")]
-//    public GameObject Cube4;
+    [Header("Image Cube 3")]
+    public GameObject Cube3;
 
-//    [Header("Cubes gris")]
-//    public GameObject CubeGris1;
-//    public GameObject CubeGris2;
+    [Header("Image Cube 4")]
+    public GameObject Cube4;
 
-//    [Header("Animation cube")]
-//    public GameObject AnimationCube3;
-//    public GameObject AnimationCube4;
+    [Header("Animation cube")]
+    public GameObject AnimationCube3;
+    public GameObject AnimationCube4;
 
-//    private List<GameObject> baseCube1;
-//    private List<GameObject> baseCube2;
+    private List<GameObject> baseCube1;
+    private List<GameObject> baseCube2;
 
-//    private List<GameObject> poolCube1;
-//    private List<GameObject> poolCube2;
+    private List<GameObject> poolCube1;
+    private List<GameObject> poolCube2;
 
-//    void Start()
-//    {
-//        Cube3.SetActive(false);
-//        Cube4.SetActive(false);
+    void Start()
+    {
+        Cube3.SetActive(false);
+        Cube4.SetActive(false);
 
-//        ChiffreCube3.SetActive(false);
-//        Chiffre2Cube3.SetActive(false);
+        ChiffreCube3.SetActive(false);
+        ChiffreCube4.SetActive(false);
 
-//        ChiffreCube4.SetActive(false);
-//        Chiffre2Cube4.SetActive(false);
+        AnimationCube3.SetActive(false);
+        AnimationCube4.SetActive(false);
 
-//        AnimationCube3.SetActive(false);
-//        AnimationCube4.SetActive(false);
+        baseCube1 = new List<GameObject>()
+        {
+            ChiffreCube3,
+            Chiffre2Cube3,
+            Cube3
+        };
 
-//        baseCube1 = new List<GameObject>()
-//        {
-//            ChiffreCube3,
-//            Chiffre2Cube3,
-//            Cube3
-//        };
+        baseCube2 = new List<GameObject>()
+        {
+            ChiffreCube4,
+            Chiffre2Cube4,
+            Cube4
+        };
 
-//        baseCube2 = new List<GameObject>()
-//        {
-//            ChiffreCube4,
-//            Chiffre2Cube4,
-//            Cube4
-//        };
+        poolCube1 = new List<GameObject>(baseCube1);
+        poolCube2 = new List<GameObject>(baseCube2);
+    }
 
-//        poolCube1 = new List<GameObject>(baseCube1);
-//        poolCube2 = new List<GameObject>(baseCube2);
-//    }
+    public void CliquerChiffre7Cube3()
+    {
+        StartCoroutine(
+            PigerCube(
+                Chiffre7Cube3,
+                AnimationCube3,
+                poolCube1,
+                baseCube1
+            )
+        );
+    }
 
-//    public void CliquerCubeGris1()
-//    {
-//        StartCoroutine(PigerCube(CubeGris1, AnimationCube3, poolCube1, baseCube1));
-//    }
+    public void CliquerChiffre7Cube4()
+    {
+        StartCoroutine(
+            PigerCube(
+                Chiffre7Cube4,
+                AnimationCube4,
+                poolCube2,
+                baseCube2
+            )
+        );
+    }
 
-//    public void CliquerCubeGris2()
-//    {
-//        StartCoroutine(PigerCube(CubeGris2, AnimationCube4, poolCube2, baseCube2));
-//    }
+    IEnumerator PigerCube(
+        GameObject boutonClique,
+        GameObject animationCube,
+        List<GameObject> pool,
+        List<GameObject> baseList
+    )
+    {
+        boutonClique.SetActive(false);
 
-//    IEnumerator PigerCube(
-//        GameObject cubeGris,
-//        GameObject animationCube,
-//        List<GameObject> pool,
-//        List<GameObject> baseList
-//    )
-//    {
-//        cubeGris.SetActive(false);
+        animationCube.SetActive(true);
 
-//        animationCube.SetActive(true);
+        yield return new WaitForSeconds(1f);
 
-//        yield return new WaitForSeconds(1f);
+        animationCube.SetActive(false);
 
-//        animationCube.SetActive(false);
+        foreach (GameObject obj in baseList)
+            obj.SetActive(false);
 
-//        foreach (GameObject obj in baseList)
-//            obj.SetActive(false);
+        if (pool.Count == 0)
+        {
+            pool.AddRange(baseList);
+        }
 
-//        if (pool.Count == 0)
-//        {
-//            pool.AddRange(baseList);
-//        }
+        if (pool.Count == 0)
+            yield break;
 
-//        if (pool.Count == 0)
-//        {
-//            yield break;
-//        }
+        int index = Random.Range(0, pool.Count);
+        GameObject result = pool[index];
 
-//        int index = Random.Range(0, pool.Count);
+        if (result != null)
+            result.SetActive(true);
 
-//        GameObject result = pool[index];
+        pool.RemoveAt(index);
 
-//        if (result != null)
-//            result.SetActive(true);
+        Debug.Log(result.name);
 
-//        pool.RemoveAt(index);
+        VerifierVictoire();
+    }
 
-//        Debug.Log(result.name);
+    void VerifierVictoire()
+    {
+        bool cube3Visible = Cube3.activeSelf;
+        bool cube4Visible = Cube4.activeSelf;
 
-//        VerifierVictoire();
-//    }
+        bool chiffresInvisible =
+            !ChiffreCube3.activeSelf &&
+            !Chiffre2Cube3.activeSelf &&
+            !ChiffreCube4.activeSelf &&
+            !Chiffre2Cube4.activeSelf;
 
-//    void VerifierVictoire()
-//    {
-//        bool cube3Visible = Cube3.activeSelf;
-//        bool cube4Visible = Cube4.activeSelf;
+        if (cube3Visible && cube4Visible && chiffresInvisible)
+        {
+            Debug.Log("reussi");
 
-//        bool chiffresInvisible =
-//            !ChiffreCube3.activeSelf &&
-//            !Chiffre2Cube3.activeSelf &&
-//            !ChiffreCube4.activeSelf &&
-//            !Chiffre2Cube4.activeSelf;
+            Chiffre7Cube3.SetActive(false);
+            Chiffre7Cube4.SetActive(false);
 
-//        if (cube3Visible && cube4Visible && chiffresInvisible)
-//        {
-//            Debug.Log("reussi");
-//            Invoke("FermerCanvas", 3f);
-//        }
-//    }
-//    x
-//    void FermerCanvas()
-//    {
-//        CanvasPuzzle8.SetActive(false);
-//    }
-//}
+            Invoke("FermerCanvas", 3f);
+        }
+    }
+
+    void FermerCanvas()
+    {
+        CanvasPuzzle8.SetActive(false);
+    }
+}
--- Assets/script/scriptBoss/Timer.cs
+++ Assets/script/scriptBoss/Timer.cs
@@ -3,30 +3,58 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Threading;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI cooldownText;
-    [SerializeField] float tempsRestant;
+    public float TempsTimer = 60f;
+    public TextMeshProUGUI TimerNombre;
+
+    [SerializeField] private Image vieNova;
+    public Canvas canva;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (tempsRestant > 0 )
+        
+        TimerUI();
+        if(TempsTimer >0)
         {
-            tempsRestant -= Time.deltaTime;
+            TempsTimer -= Time.deltaTime;
+            TempsTimer = Mathf.Round(TempsTimer * 100.0f) * 0.01f;
+
         }
-        else
-        {
-            Debug.Log("fin timer");
+        if(TempsTimer <= 0){
+            TempsTimer = 0f;
+            Invoke("fermerUI", 1f);
         }
 
+    }
+
+    void TimerUI()
+    {
+        TimerNombre.text = TempsTimer.ToString();
+    }
+
+    void fermerUI()
+    {
+        canva.enabled = false;
+        Invoke("prendreDegats", 1f);
+    }
+
+    void prendreDegats()
+    {
+        vieNova.fillAmount -= 0.1f;
+        Invoke("Reset", 0f);
+    }
+
+    private void Reset()
+    {
+        Debug.Log("RESET");
     }
 }
