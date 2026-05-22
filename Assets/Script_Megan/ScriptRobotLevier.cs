9027adf719a5677bd36e396b59b3f2123af29144
Megan-Desjardins <e6248868@cmaisonneuve.qc.ca>
2026-05-22 01:26:02 -0400

    Puzzle levier robot 2

--- Assets/Script_Megan/ScriptRobotLevier.cs
+++ Assets/Script_Megan/ScriptRobotLevier.cs
@@ -1,29 +1,75 @@
 //------------------------------------------*
 // Author : Megan Desjardins
 // Description : Ce script permet de déplacer le
-// levier quand on clique
+// levier quand on clique et de déplacer le robot
 //------------------------------------------*
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScriptRobotLevier : MonoBehaviour
 {
-    public Canvas canvaLevierTuto;
+    //------------------------------------------*
+    // VARIABLES
+    //------------------------------------------*
     public Button levier;
+    public Image levierBase;
     public Animator animTranslation;
     public GameObject robot;
-    public GameObject positionRobotApresLevier;//mettre un gameobject empty avec la position du robot qui a bougé
+    bool playerClose = false;
+    // Mettre un gameobject empty avec la position du
+    // robot qui a bougé
+    public GameObject positionRobotApresLevier;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //------------------------------------------*
+    // FONCTIONS
+    //------------------------------------------*
+
+    // Start
     void Start()
     {
+        // Désactiver les les éléments au début
         levier = levier.GetComponent<Button>();
+        levier.gameObject.SetActive(false);
+        levierBase.gameObject.SetActive(false);
         animTranslation.enabled = false;
     }
 
-    public void clickRotation()
+    // Update
+    void Update()
+    {
+        // Si on click sur E et que Nova est dans le collider
+        if(playerClose && Input.GetKeyDown(KeyCode.E))
+        {
+            //Debug.Log("Puzzle s'affiche");
+            levier.gameObject.SetActive(true);
+            levierBase.gameObject.SetActive(true);
+        }
+    }
+
+    //------------------------------------------*
+    // Fonction PiecePlaced
+    // Description : Cette fonction lance l'animation du
+    // levier et change la position du robot quand on click
+    //------------------------------------------*
+    public void clickTranslation()
     {
         animTranslation.enabled = true;
-        // Le robot translate ici
+        robot.transform.position = positionRobotApresLevier.transform.position;
+    }
+
+    //------------------------------------------*
+    // COLLISIONS
+    // Description : Si Nova entre dans le Collider
+    // autour du PuzzleRobotManager
+    //------------------------------------------*
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        // --- Ajouter le tag "Nova" au perso *
+        if(other.CompareTag("Nova"))
+        {
+            //Debug.Log("Nova est proche");
+            playerClose = true;
+        }
     }
 }
