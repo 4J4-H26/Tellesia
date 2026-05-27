3da9473be6e543f98a64ef2b1e697072e6fb83c6
Celian136 <pianosel173@gmail.com>
2026-05-27 00:49:35 -0400

    Scène Mort - Boutton quitter

---
+++ Assets/Scripts/Niveau-5/QuitterRetourMenuPrincipal.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitterRetourMenuPrincipal : MonoBehaviour
+{
+
+    public void LoadScene() 
+    {
+        SceneManager.LoadScene("Niveau1");
+    }
+}
